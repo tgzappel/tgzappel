5f6faf4290407e5dca914902a212f49a0b8e3359
tgzappel <jclicosmos@gmail.com>
2026-02-11 19:56:35 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const DATA = {
+    levels: [
+        { id: "atsc", name: "Among the Stars C-Side", points: 2000, img: "atsc.png", code: "123..." },
+        { id: "tic", name: "The Instant Cheeseless", points: 1750, img: "tic.png", code: "456..." },
+        // If you add a level here, you don't need to change any IDs!
+    ],
+    players: [
+        { name: "Grassy", completions: ["atsc", "tic"] },
+        { name: "AppelPro", completions: ["tic"] }
+    ]
+};
